e1fd0d39cadec088c25cb70e784eae6f130e1a29
Vishal Rathore <vishal.rathore@multitech.com>
2023-07-09 01:52:40 -0500

    small refactor to extract datareader in it's own file

---
+++ src/DataReader.ts
@@ -0,0 +1,19 @@
+/**
+ * Interface for classes that can read and parse data from files.
+ */
+export interface DataReader {
+  /**
+   * Reads the content of a file and returns a promise that resolves with the content as a string.
+   * @param file The file to read.
+   * @returns A promise that resolves with the content of the file as a string.
+   */
+  readFile: (file: File) => Promise<string>;
+
+  /**
+   * Parses a string into a 2D array where each inner array represents a row of the CSV file,
+   * and each item in the row represents a cell in the CSV.
+   * @param contents The string to parse.
+   * @returns A 2D array where each inner array represents a row of the CSV file.
+   */
+  parseCSV: (contents: string) => string[][];
+}
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,9 +1,10 @@
 import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MatchResult";
+import { DataReader } from "./DataReader";
 
 /**
- * MatchData represents a match in the CSV file. It includes a Date, 
- * two strings for teams, two numbers for goals, a MatchResult for match result, 
+ * MatchData represents a match in the CSV file. It includes a Date,
+ * two strings for teams, two numbers for goals, a MatchResult for match result,
  * and a string for referee.
  */
 export type MatchData = [
@@ -15,26 +16,6 @@ export type MatchData = [
   MatchResult, // The result of the match.
   string // The referee of the match.
 ];
-
-/**
- * Interface for classes that can read and parse data from files.
- */
-interface DataReader {
-  /**
-   * Reads the content of a file and returns a promise that resolves with the content as a string.
-   * @param file The file to read.
-   * @returns A promise that resolves with the content of the file as a string.
-   */
-  readFile: (file: File) => Promise<string>;
-
-  /**
-   * Parses a string into a 2D array where each inner array represents a row of the CSV file,
-   * and each item in the row represents a cell in the CSV.
-   * @param contents The string to parse.
-   * @returns A 2D array where each inner array represents a row of the CSV file.
-   */
-  parseCSV: (contents: string) => string[][];
-}
 
 /**
  * Class for reading and processing match data.
